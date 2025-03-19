6387bada1b56912a621233c0729336ad86547692
vanloc2003 <dangvanloc2003@gmail.com>
2025-03-19 16:57:02 +0700

    day 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShoeShop from "./Render_shop/ShoeShop";
+
+const App = () => {
+  return (
+    <div>
+      <ShoeShop />
+    </div>
+  );
+};
+
+export default App;
